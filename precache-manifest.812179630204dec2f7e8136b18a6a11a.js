e7c79054fa912ac7e27b318801baf713e46c6435
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-05-31 06:25:45 +0000

    Deploy katrimarika/timetables to github.com/katrimarika/timetables.git:gh-pages

---
+++ precache-manifest.812179630204dec2f7e8136b18a6a11a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "027e28775e3a758a77eb77b279740a39",
+    "url": "/index.html"
+  },
+  {
+    "revision": "65564311ad736a55c984",
+    "url": "/static/css/main.543da1ad.chunk.css"
+  },
+  {
+    "revision": "9779a875dd2e5bd75587",
+    "url": "/static/js/2.63f57fe4.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/static/js/2.63f57fe4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65564311ad736a55c984",
+    "url": "/static/js/main.09c9bff5.chunk.js"
+  },
+  {
+    "revision": "52419ac0ef6746e062dc",
+    "url": "/static/js/runtime-main.2af4d594.js"
+  }
+]);
